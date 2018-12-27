2d5ef50c5e4e150019a642e4ddfde6a32da9964a
said shah <saidmysr20@aol.com>
2018-12-26 22:06:59 -0500

    added controller for creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,21 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def create
+      @user = User.create(createUserParams)
+      if @user.valid?
+        token= JWT.encode({user_id: @user.id}, '')
+        render json: {user: @user, jwt: token}
+      else
+        render json: {error: "WRONG"},status: 422
+      end
+
+  end
+
+private
+
+def createUserParams
+  params.require(:user).permit(:first_name,:last_name,:house_number,:street_name,:city,:state,:zipcode,:date_of_birth,:username,:password,:email)
+end
+
 end
