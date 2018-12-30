46fc924b6e939e1f0207a472ef49a92cce47d7c3
said shah <saidmysr20@aol.com>
2018-12-30 15:19:43 -0500

    added nested routes for users and stocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-  resources :stocks
+  resources :users do
+   resources :stocks
+  end
+
   get '/current_user', to: "auth#show"
   post '/login', to: "auth#create"
 end
