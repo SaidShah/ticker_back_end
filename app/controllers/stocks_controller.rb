78257573e1e73cb22aa55426d3742d8ea7c0ca0e
said shah <saidmysr20@aol.com>
2018-12-31 17:06:35 -0500

    added method to create stock for user

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -19,7 +19,11 @@ class StocksController < ApplicationController
   end
 
   def create
-    byebug
+    @user = User.find(create_stock[:user_id])
+    @newStock = Stock.create(symbol: create_stock[:symbol],total_quantity: create_stock[:quantity], total_value: create_stock[:total_value], purchase_price: create_stock[:purchase_price])
+    UserStock.create(user_id: create_stock[:user_id], stock_id: @newStock.id)
+    render json: @user
+    
   end
 
 
@@ -27,6 +31,10 @@ private
 
   def new_stock_values
     params.require(:values).permit(:total_value, :new_stock_count, :new_account_balance, :new_value_for_stock, :stock_id, :user_id)
+  end
+
+  def create_stock
+    params.require(:values).permit(:total_value, :total_funds, :symbol, :user_id, :quantity, :purchase_price)
   end
 
 end
